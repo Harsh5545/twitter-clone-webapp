9847eb50f903a1bde7be75860e42c9bc35de3a02
harshad <Harshadkajale333@gmail.com>
2023-02-05 13:22:12 +0530

    new twitter app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
 import { useState } from "react";
-import style from "./App.module.css";
-import Input from "./atoms/Input/Input";
 
-function App() {
-  const [check, setCheck]= useState("")
-  function handle(e) {
-   setCheck(e.target.value)
- }
-  return (
-    <div className="App">
-      <Input label="hasrh" handleChange={handle} type="password"  />
-   {check}
-    </div>
-  );
-}
+
+function App() {}
 
 export default App;
