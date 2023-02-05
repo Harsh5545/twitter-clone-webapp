bbb6687e0d8a7c7369ffa958577129799183deee
harshad <Harshadkajale333@gmail.com>
2023-02-05 17:47:38 +0530

    updated

--- src/component/signIn/SignInForm.js
+++ src/component/signIn/SignInForm.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react'
 import CustomInput from '../../atoms/Input/CustomInput'
-
+import { Dialog } from '@mui/material'
 function SignInForm() {
    
   return (
       <>
+          <Dialog>
           <CustomInput label="Phone, Email, or Username"/>
+
+
+          </Dialog>
+         
      
       </>
   )
